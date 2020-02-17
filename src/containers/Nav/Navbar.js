f2da44b23217017531a932f1ef7c5cb81b0bc673
Rhayven <codingforllamas@gmail.com>
2020-02-17 16:45:05 -0500

    Changes to navbar

--- src/containers/Nav/Navbar.js
+++ src/containers/Nav/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 // import ButtonContainer from './Buttons/Button';
 // import ButtonToggler from './Buttons/ButtonToggler';
 
@@ -8,12 +8,12 @@ const Navbar = () => (
     <div className="menu-branding">
       <div className="portrait" role="img" aria-label="Small Web Developer Portrait" />
     </div>
-    <div className="menu-nav">
-      <Link to="/" className="nav-item"><div className="nav-link">Home</div></Link>
-      <Link to="/about" className="nav-item"><div className="nav-link">About Me</div></Link>
-      <Link to="/work" className="nav-item"><div className="nav-link">My Work</div></Link>
-      <Link to="/contact" className="nav-item"><div className="nav-link">Contact Me</div></Link>
-    </div>
+    <ul className="menu-nav">
+      <li className="nav-item"><NavLink to="/" className="nav-link" exact activeClassName="active">Home</NavLink></li>
+      <li className="nav-item"><NavLink to="/about" className="nav-link">About Me</NavLink></li>
+      <li className="nav-item"><NavLink to="/work" className="nav-link">My Work</NavLink></li>
+      <li className="nav-item"><NavLink to="/contact" className="nav-link">Contact Me</NavLink></li>
+    </ul>
   </nav>
 );
 
