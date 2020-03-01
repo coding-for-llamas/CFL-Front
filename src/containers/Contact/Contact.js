c987fe4858d79589c483432a91577cc476cc602d
Rhayven <codingforllamas@gmail.com>
2020-03-01 04:07:10 -0500

    Refactoring and adds contact page

---
+++ src/containers/Contact/Contact.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import commonUtils from '../../components/lib/commonUtils';
+import DefaultFooter from '../../components/Footer/Footer';
+
+export class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.commonUtils = commonUtils;
+  }
+
+  componentDidMount() { this.commonUtils.setTitleAndScroll('Contact Me'); }
+
+  render() {
+    return (
+      <div className="page-content">
+        <main id="contact">
+          <h1 className="lg-heading">
+                  Contact
+            {' '}
+            <span className="text-secondary">Me</span>
+          </h1>
+
+          <h2 className="sm-heading">
+                  Want to talk? Here&apos;s where I can be reached!
+          </h2>
+
+          <div className="contact-info">
+            <div className="contact contact-1">
+              <p>
+                <a href="mailto:rhayven@codingforllamas.com">Email</a>
+                <br />
+                <a href="https://www.facebook.com/codingforllamas/">Facebook</a>
+                <br />
+                <a href="https://twitter.com/codingforllamas">Twitter</a>
+                <br />
+                <a href="https://github.com/unesdala">Github</a>
+              </p>
+            </div>
+          </div>
+        </main>
+        <DefaultFooter />
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- src/containers/Work/Work.js
+++ src/containers/Work/Work.js
@@ -1,62 +1,47 @@
 import React, { Component } from 'react';
-import ReactResizeDetector from 'react-resize-detector';
 import commonUtils from '../../components/lib/commonUtils';
+import Footer from '../../components/Footer/Footer';
 import project1 from '../../../static/img/projects/project1.png';
 
-export class About extends Component {
+export class Work extends Component {
   constructor(props) {
     super(props);
     this.commonUtils = commonUtils;
-    this.parentRef = React.createRef();
-    this.onResize = this.onResize.bind(this);
-    this.state = { width: 100 };
   }
 
-  onResize(width) { this.setState({ width }); }
+  componentDidMount() { this.commonUtils.setTitleAndScroll('My Work'); }
 
   render() {
-    const { width } = this.state;
     return (
       <div className="page-content">
-        {width >= 1004
-          ? (
-            <main id="work">
-              <h1 className="lg-heading">
+        <main id="work">
+          <h1 className="lg-heading">
                 My
-                {' '}
-                <span className="text-secondary">Work</span>
-              </h1>
+            {' '}
+            <span className="text-secondary">Work</span>
+          </h1>
 
-              <h2 className="sm-heading">
+          <h2 className="sm-heading">
                 Some of the projects I&apos;ve worked on.
-              </h2>
+          </h2>
 
-              <div className="projects">
-                <div className="item">
-                  <a href="/daycare">
-                    <img src={project1} alt="Screenshot of a daycare website" />
-                  </a>
-                  <a href="/daycare" className="btn-light" aria-label="Link to Caring Child Daycare">
-                    <i className="fas fa-eye" />
-                    {' '}
+          <div className="projects">
+            <div className="item">
+              <a href="/daycare">
+                <img src={project1} alt="Screenshot of a daycare website" />
+              </a>
+              <a href="/daycare" className="btn-light" aria-label="Link to Caring Child Daycare">
+                <i className="fas fa-eye" />
+                {' '}
                     Caring Child Daycare
-                  </a>
-                </div>
-              </div>
-
-            </main>
-          )
-          : (
-            <div className="page-content">
-              <p>
-                Hello from Narrow Work Page.
-              </p>
+              </a>
             </div>
-          )}
-        <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} targetDomEl={this.parentRef.current} />
+          </div>
+        </main>
+        <Footer />
       </div>
     );
   }
 }
 
-export default About;
+export default Work;
