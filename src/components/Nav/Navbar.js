e8c24fceb2fd8ea034d182db99b53e0c56af68a0
Rhayven <codingforllamas@gmail.com>
2020-03-02 05:23:09 -0500

    Adds mobile nav and fixes styling

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-// import ButtonContainer from './Buttons/Button';
-// import ButtonToggler from './Buttons/ButtonToggler';
+import ReactResizeDetector from 'react-resize-detector';
+import DefaultCollapsableNav from './CollapsableNav';
 
-const Navbar = () => (
-  <nav className="menu">
-    <div className="menu-branding">
-      <div className="portrait" role="img" aria-label="Small Web Developer Portrait" />
-    </div>
-    <ul className="menu-nav">
-      <li className="nav-item"><NavLink to="/" className="nav-link" exact activeClassName="active">Home</NavLink></li>
-      <li className="nav-item"><NavLink to="/about" className="nav-link">About Me</NavLink></li>
-      <li className="nav-item"><NavLink to="/work" className="nav-link">My Work</NavLink></li>
-      <li className="nav-item"><NavLink to="/contact" className="nav-link">Contact Me</NavLink></li>
-    </ul>
-  </nav>
-);
+export default class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.parentRef = React.createRef();
+    this.onResize = this.onResize.bind(this);
+    this.state = { width: 320 };
+  }
 
-export default Navbar;
+  onResize(width) {
+    this.setState({ width });
+  }
+
+  render() {
+    const { width } = this.state;
+    return (
+      <>
+        {width <= 768 - 18 || width === 751
+          ? (
+            <DefaultCollapsableNav />
+          ) : (
+            <nav className="menu">
+              <ul className="menu-nav">
+                <li className="nav-item"><NavLink to="/" className="nav-link" exact activeClassName="active">Home</NavLink></li>
+                <li className="nav-item"><NavLink to="/about" className="nav-link">About Me</NavLink></li>
+                <li className="nav-item"><NavLink to="/work" className="nav-link">My Work</NavLink></li>
+                <li className="nav-item"><NavLink to="/contact" className="nav-link">Contact Me</NavLink></li>
+              </ul>
+            </nav>
+          )}
+        <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} targetDomEl={this.parentRef.current} />
+      </>
+    );
+  }
+}
