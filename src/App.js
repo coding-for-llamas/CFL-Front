1bb7d6c0ff21bd7ac360e66ecdc5f5e98b1da611
Rhayven <codingforllamas@gmail.com>
2020-03-04 02:52:03 -0500

    Adds imageActions and connects App.js to redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import Navbar from './components/Nav/Navbar';
 import AppTemplate from './components/App-Main';
 import DefaultAbout from './containers/About/About';
 import DefaultWork from './containers/Work/Work';
 import DefaultContact from './containers/Contact/Contact';
 import FourOhFour from './containers/404';
+import getImages from './actions/imageActions';
+import mapStoreToProps from './redux/mapStoreToProps';
 
-export class App extends PureComponent {
+export class App extends Component {
+  async componentDidMount() {
+    const { dispatch, images } = this.props;
+    if (images.length === 0)dispatch(getImages());
+  }
+
   render() {
     return (
       <div id="App" className="App Site">
@@ -29,4 +40,10 @@ export class App extends PureComponent {
   }
 }
 
-export default App;
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  images: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.shape({})), PropTypes.shape({})]),
+};
+App.defaultProps = { images: [] };
+
+export default connect(mapStoreToProps, null)(App);
---
+++ src/actions/imageActions.js
@@ -0,0 +1,30 @@
+import request from 'superagent';
+
+export const fetchImages = () => ({
+  type: 'FETCH_IMAGES',
+});
+
+export const receiveImages = (docs) => ({
+  type: 'FETCHED_IMAGES',
+  data: docs,
+});
+
+export const receiveError = (e) => ({
+  type: 'RECEIVE_ERROR',
+  error: e,
+});
+
+const getImages = () => (dispatch, getState) => {
+  const { images } = getState();
+  const type = 'img';
+  if (images.images !== undefined && images.images.length > 0) return Promise.resolve(true);
+  dispatch(fetchImages());
+  return request.get(`${process.env.BackendUrl}/documents?type=${type}`).set('Accept', 'application/json')
+    .then((data) => {
+      if (data.body.message === 'Not Found') return dispatch(receiveError(new Error('No pictures found!!')));
+      return dispatch(receiveImages(data.body));
+    })
+    .catch((err) => dispatch(receiveError(err)));
+};
+
+export default getImages;
