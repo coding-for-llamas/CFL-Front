9648c26261f3e283f3a6997c9f972f58af6a37c9
Rhayven <codingforllamas@gmail.com>
2020-07-26 06:24:48 -0400

    Refactors content && fixes homepage SCSS

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import commonUtils from '../../components/lib/commonUtils';
+import DefaultContactContent from './ContactContent';
 import DefaultFooter from '../../components/Footer/Footer';
 
 export class Contact extends Component {
@@ -12,34 +13,12 @@ export class Contact extends Component {
 
   render() {
     return (
-      <div className="page-content">
-        <main id="contact">
-          <h1 className="lg-heading">
-            Contact
-            {' '}
-            <span className="text-secondary">Me</span>
-          </h1>
-
-          <h2 className="sm-heading">
-            Want to talk? Here&apos;s where I can be reached!
-          </h2>
-
-          <div className="contact-info">
-            <div className="contact contact-1">
-              <p>
-                <a href="mailto:rhayven@codingforllamas.com">Email</a>
-                <br />
-                <a href="https://www.facebook.com/codingforllamas/">Facebook</a>
-                <br />
-                <a href="https://twitter.com/codingforllamas">Twitter</a>
-                <br />
-                <a href="https://github.com/unesdala">Github</a>
-              </p>
-            </div>
-          </div>
-        </main>
+      <>
+        <div className="page-content">
+          <DefaultContactContent />
+        </div>
         <DefaultFooter />
-      </div>
+      </>
     );
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../static/css/main.scss';
 import DefaultApp from './App';
 import store from './redux/store/index';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../static/css/main.scss';
 
 render(
   <Provider store={store.store}>
