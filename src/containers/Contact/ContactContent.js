8445416a1eb4778e75ad08392f36be4fa7696bc4
Rhayven <codingforllamas@gmail.com>
2020-07-26 21:59:02 -0400

    Final updates

--- src/containers/Contact/ContactContent.js
+++ src/containers/Contact/ContactContent.js
@@ -1,31 +1,33 @@
 import React from 'react';
 
 const ContactContent = () => (
-  <main className="contact">
-    <h1 className="lg-heading">
-      Contact
-      {' '}
-      <span className="text-secondary">Me</span>
-    </h1>
+  <div className="container">
+    <main className="contact">
+      <h1 className="lg-heading">
+        Contact
+        {' '}
+        <span className="text-secondary">Me</span>
+      </h1>
 
-    <h2 className="sm-heading">
-      Want to talk? Here&apos;s where I can be reached!
-    </h2>
+      <h2 className="sm-heading">
+        Want to talk? Here&apos;s where I can be reached!
+      </h2>
 
-    <div className="contact-info">
-      <div className="contact contact-1">
-        <p>
-          <a href="mailto:rhayven@codingforllamas.com">Email</a>
-          <br />
-          <a href="https://www.facebook.com/codingforllamas/">Facebook</a>
-          <br />
-          <a href="https://twitter.com/codingforllamas">Twitter</a>
-          <br />
-          <a href="https://github.com/unesdala">Github</a>
-        </p>
+      <div className="contact-info">
+        <div className="contact contact-1">
+          <p>
+            <a href="mailto:rhayven@codingforllamas.com">Email</a>
+            <br />
+            <a href="https://www.facebook.com/codingforllamas/">Facebook</a>
+            <br />
+            <a href="https://twitter.com/codingforllamas">Twitter</a>
+            <br />
+            <a href="https://github.com/unesdala">Github</a>
+          </p>
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
+  </div>
 );
 
 export default ContactContent;
