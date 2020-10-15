0e5a4e6261beac8824afc5d94516ad97c5b69c90
Rhayven <codingforllamas@gmail.com>
2020-10-14 23:11:12 -0400

    Test fixes

---
+++ test/main.spec.js
@@ -0,0 +1,9 @@
+import * as main from '../src/main';
+
+describe('Main', () => {
+  it('renders to the root', () => {
+    expect(document.getElementById('root')).not.toBe(null);
+    expect(document.getElementById('root')).not.toBe(undefined);
+    expect(main).toBeDefined();
+  });
+});
