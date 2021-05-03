f5a32429981eed821a605c85e0d01823b7049db2
Rhayven <codingforllamas@gmail.com>
2021-05-03 03:57:53 -0400

    Fixes puppeteer

--- test/jest-pretest.ts
+++ test/jest-pretest.ts
@@ -1,12 +1,14 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { config } from 'dotenv';
 
+config();
 configure({ adapter: new Adapter() });
 document.body.innerHTML = '<div id="root"></div>';
-
+Object.defineProperty(window, 'location', { value: { reload: jest.fn(), assign: jest.fn(), href: '/' }, writable: true });
 window.location = {
   ...window.location,
   href: 'https://codingforllamas.com',
-  reload: jest.fn(),
-  assign: jest.fn(),
+  reload: () => {},
+  assign: () => {},
 };
--- test/puppeteer/home.spec.tsx
+++ test/puppeteer/home.spec.tsx
@@ -4,7 +4,7 @@ import 'regenerator-runtime/runtime';
 describe('Home', () => {
   it('should be titled "Coding For Llamas"', async () => {
     const page = await browser.newPage();
-    await page.goto('http://localhost:8989/', {
+    await page.goto('http://localhost:8999/', {
       waitUntil: 'load',
     });
     await expect(page.title()).resolves.toMatch('Coding For Llamas');
