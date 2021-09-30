7ee99ee5b07386fcf0a9412d71f0140ab81c7b37
Rhayven <codingforllamas@gmail.com>
2021-09-30 16:51:25 -0400

    Adding case study from coursera

--- src/containers/Work/WorkContent.tsx
+++ src/containers/Work/WorkContent.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const project1 = 'https://www.dl.dropboxusercontent.com/s/c9mxoixh5sqnict/project1.png?dl=0';
+const project2 = 'https://dl.dropboxusercontent.com/s/d652fnqcve9u3bb/Screenshot%202021-09-30%20at%2016-34-10%20cfl-front%20-%20Dropbox.png?dl=0';
+const project2PDF = 'https://dl.dropboxusercontent.com/s/o7vvnotuy1y6lh3/Cafe%20Latte%20Case%20Study.pdf?dl=0';
 
 const WorkContent = (): JSX.Element => (
   <div className="container">
@@ -33,6 +35,16 @@ const WorkContent = (): JSX.Element => (
             <i className="fab fa-github" />
             &nbsp;
             Github
+          </a>
+        </div>
+        <div className="item">
+          <a href={project2PDF}>
+            <img src={project2} alt="Screenshot of the cafe latte PDF preview" />
+          </a>
+          <a href={project2PDF} className="btn-light" aria-label="Link to the PDF of the Cafe Latte case study">
+            <i className="fas fa-eye" />
+            &nbsp;
+            Cafe Latte Case Study
           </a>
         </div>
       </div>
