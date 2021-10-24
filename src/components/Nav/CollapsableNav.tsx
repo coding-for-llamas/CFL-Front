8ffcd6c072bcc3b607a872d03fa9c566065eb49f
Rhayven <codingforllamas@gmail.com>
2021-10-23 22:34:15 -0400

    Added resume to navbar

--- src/components/Nav/CollapsableNav.tsx
+++ src/components/Nav/CollapsableNav.tsx
@@ -19,6 +19,15 @@ const CollapsableNav = (): JSX.Element => {
             <NavItem><NavLink to="/about" tag={RRNavLink} onClick={toggleNavbar}>About Me</NavLink></NavItem>
             <NavItem><NavLink to="/work" tag={RRNavLink} onClick={toggleNavbar}>My Work</NavLink></NavItem>
             <NavItem><NavLink to="/contact" tag={RRNavLink} onClick={toggleNavbar}>Contact Me</NavLink></NavItem>
+            <NavItem>
+              <NavLink
+                to="https://dl.dropboxusercontent.com/s/y47jfwxu6lytoas/Rhayven_Ayers_Resume.pdf?dl=0"
+                tag={RRNavLink}
+                onClick={toggleNavbar}
+              >
+                Resume
+              </NavLink>
+            </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -35,6 +35,16 @@ export default class Navigation extends Component<Record<string, unknown>, Navig
                 <li className="nav-item"><NavLink to="/about" className="nav-link">About Me</NavLink></li>
                 <li className="nav-item"><NavLink to="/work" className="nav-link">My Work</NavLink></li>
                 <li className="nav-item"><NavLink to="/contact" className="nav-link">Contact Me</NavLink></li>
+                <li className="nav-item">
+                  <a
+                    href="https://dl.dropboxusercontent.com/s/y47jfwxu6lytoas/Rhayven_Ayers_Resume.pdf?dl=0"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="nav-link"
+                  >
+                    Resume
+                  </a>
+                </li>
               </ul>
             </nav>
           )}
