85f677213f3bf835d07bc056af282cb07cc653c3
Rhayven <codingforllamas@gmail.com>
2022-01-14 20:17:51 -0500

    Work content styling

--- src/containers/Contact/ContactContent.tsx
+++ src/containers/Contact/ContactContent.tsx
@@ -1,14 +1,16 @@
 const ContactContent = (): JSX.Element => (
-  <div className="Contact-header">
-    <h1 className="lg-heading">
-      Contact
-      {' '}
-      <span className="text-secondary">Me</span>
-    </h1>
-    <h2 className="sm-heading">
-      Want to talk? Here's where I can be reached!
-    </h2>
-  </div>
+  <>
+    <div className="Contact-header">
+      <h1 className="lg-heading">
+        Contact
+        {' '}
+        <span className="text-secondary">Me</span>
+      </h1>
+      <h2 className="sm-heading">
+        Want to talk? Here's where I can be reached!
+      </h2>
+    </div>
+  </>
 );
 
 export default ContactContent;
