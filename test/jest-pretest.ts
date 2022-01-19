e971caca60e11cf774484d37bcd5caad9849c64c
Rhayven <codingforllamas@gmail.com>
2022-01-19 01:39:03 -0500

    Changes url for jest pretest

--- test/jest-pretest.ts
+++ test/jest-pretest.ts
@@ -22,7 +22,7 @@ window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
 Object.defineProperty(window, 'location', { value: { assign: () => { }, reload: () => { } }, writable: true });
 window.location = {
   ...window.location,
-  href: 'https://web-jam.com',
+  href: 'https://codingforllamas.com',
   reload: jest.fn(),
   assign: jest.fn(),
 };
